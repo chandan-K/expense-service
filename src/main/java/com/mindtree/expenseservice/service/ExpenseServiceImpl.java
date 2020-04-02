ae5d92ac93d4b4ecc431bf6abc5dab6fe1a1975a
chandan.kumar@sita.aero <chandan.kumar@mindtree.com>
2020-04-02 13:23:10 +0100

    added itext support for PDF support.

--- src/main/java/com/mindtree/expenseservice/service/ExpenseServiceImpl.java
+++ src/main/java/com/mindtree/expenseservice/service/ExpenseServiceImpl.java
@@ -2,6 +2,7 @@ package com.mindtree.expenseservice.service;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -12,6 +13,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.ImageRenderInfo;
+import com.itextpdf.text.pdf.parser.PdfImageObject;
+import com.itextpdf.text.pdf.parser.PdfReaderContentParser;
+import com.itextpdf.text.pdf.parser.RenderListener;
+import com.itextpdf.text.pdf.parser.TextRenderInfo;
 import com.mindtree.expenseservice.dto.ExpenseDTO;
 import com.mindtree.expenseservice.dto.InvoiceInfo;
 import com.mindtree.expenseservice.dto.InvoiceInfoRequest;
@@ -27,11 +34,22 @@ public class ExpenseServiceImpl implements ExpenseService {
 	private String datapath;
 	@Override
 	public InvoiceInfo getInvoiceInfo(InvoiceInfoRequest invoiceInfoReq) throws TesseractException {
-		ExpenseDTO expense = invoiceInfoReq.getExpenseDTO();
 
+		InvoiceInfo invoiceInfoRes = readPDFUsingIText(invoiceInfoReq);
+
+		if (invoiceInfoRes.getAmmount() == null || invoiceInfoRes.getDate() == null || invoiceInfoRes.getMerchant() == null
+				|| invoiceInfoRes.getTxnId() == null) {
+			 readPDFUsingTesseract(invoiceInfoReq, invoiceInfoRes);
+		}
+		return invoiceInfoRes;
+	}
+
+	private void readPDFUsingTesseract(InvoiceInfoRequest invoiceInfoReq, InvoiceInfo invoiceInfoRes) throws TesseractException {
+		ExpenseDTO expense = invoiceInfoReq.getExpenseDTO();
+/*
 		if (expense.getFileName() == null && invoiceInfoReq.getByteStream() == null) {
 			return null;
-		}
+		}*/
 		String fileName = expense.getFileName();
 		if (fileName == null) {
 			fileName = "tmp.pdf";
@@ -41,7 +59,7 @@ public class ExpenseServiceImpl implements ExpenseService {
 			decoder = Base64.getDecoder().decode(expense.getDocument().split(";base64,")[1]);
 		}
 		String property = System.getProperty("java.io.tmpdir");
-        File tempFile = new File(property + "/" + fileName);
+		File tempFile = new File(property + "/" + fileName);
 		try (FileOutputStream fos = new FileOutputStream(tempFile);) {
 			fos.write(decoder);
 
@@ -52,45 +70,134 @@ public class ExpenseServiceImpl implements ExpenseService {
 		try {
 			//URL resource = getClass().getResource(System.getProperty("java.io.tmpdir") );
 			instance.setDatapath(datapath);
-			return processImgeText(instance.doOCR(tempFile));
+			processImgeText(instance.doOCR(tempFile), invoiceInfoRes);
 		} finally {
 			tempFile.delete();
 		}
 	}
 	
-	private InvoiceInfo processImgeText(String imgText) {
+	/**
+	 * @param invoiceInfoReq 
+	 * 
+	 */
+	private InvoiceInfo readPDFUsingIText(InvoiceInfoRequest invoiceInfoReq) {
+		InvoiceInfo invoiceInfo = new InvoiceInfo();
+		byte[] decoder = null;
+		if (invoiceInfoReq.getByteStream() != null) {
+			decoder = invoiceInfoReq.getByteStream();
+		} else {
+			decoder = Base64.getDecoder().decode(invoiceInfoReq.getExpenseDTO().getDocument().split(";base64,")[1]);
+		}
+		try {
+			PdfReader reader = new PdfReader(decoder);
+			int pageILikeToCheck = reader.getNumberOfPages(); // set the page or loop them all
+
+			PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+			parser.processContent(pageILikeToCheck, new RenderListener() {
+				@Override
+				public void renderImage(ImageRenderInfo renderInfo) {
+					/*
+					 * PdfImageObject image; try { image = renderInfo.getImage(); if (image == null)
+					 * return; System.out.println("Found image");
+					 * System.out.println(renderInfo.getStartPoint()); } catch (IOException e) {
+					 * e.printStackTrace(); }
+					 */}
+
+				@Override
+				public void renderText(TextRenderInfo renderInfo) {
+					if (renderInfo.getText().length() > 0) {
+						System.out.println(renderInfo.getText());
+						processText(renderInfo.getText(), invoiceInfo);
+					}
+				}
+
+				@Override
+				public void endTextBlock() {
+				}
+
+				@Override
+				public void beginTextBlock() {
+				}
+
+			});
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return invoiceInfo;
+	}
+	 
+	 /**
+	  * processText
+	  * @param text
+	  * @param invoiceInfo
+	  */
+    private void processText(String text, InvoiceInfo invoiceInfo) {
+			if(text.startsWith("Date : ")) {
+				SimpleDateFormat sf = new SimpleDateFormat("EEE, d MMM yyyy");
+				String substring = text.substring("Date : ".length());
+				try {
+					Date parse = sf.parse(substring.trim());
+					invoiceInfo.setDate(parse);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+			
+			}
+			else if(text.startsWith("Total Price: ")) {
+				invoiceInfo.setAmmount(text.substring("Total Price: ".length()));
+			}
+			else if(text.startsWith("Ref: ")) {
+				invoiceInfo.setTxnId(text.substring("Ref: ".length()));
+			}
+			else if(text.startsWith("For ")) {
+				invoiceInfo.setMerchant(text.substring("For ".length(), text.indexOf(':') - 1).trim());
+			}
+			
+		}
+	
+	
+	
+	private void processImgeText(String imgText, InvoiceInfo invoiceInfoRes) {
 		System.out.println(imgText);
-		InvoiceInfo resp = new InvoiceInfo();
 		String[] split = imgText.split("\n");
-		Optional<String> date = Arrays.stream(split).filter(str -> str.startsWith("Date : ")).findFirst();
-		if (date.isPresent()) {
-			SimpleDateFormat sf = new SimpleDateFormat("EEE, d MMM yyyy");
-			String substring = date.get().substring("Date : ".length());
-			try {
-				Date parse = sf.parse(substring.trim());
-				resp.setDate(parse);
-			} catch (ParseException e) {
-				e.printStackTrace();
+		if (invoiceInfoRes.getDate() == null) {
+			Optional<String> date = Arrays.stream(split).filter(str -> str.startsWith("Date : ")).findFirst();
+			if (date.isPresent()) {
+				SimpleDateFormat sf = new SimpleDateFormat("EEE, d MMM yyyy");
+				String substring = date.get().substring("Date : ".length());
+				try {
+					Date parse = sf.parse(substring.trim());
+					invoiceInfoRes.setDate(parse);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 
-		Optional<String> total = Arrays.stream(split).filter(str -> str.startsWith("Total Price: ")).findFirst();
-		if (total.isPresent()) {
-			resp.setAmmount(total.get().substring("Total Price: ".length()));
-		}
-
-		Optional<String> txId = Arrays.stream(split).filter(str -> str.startsWith("Ref: ")).findFirst();
-		if (txId.isPresent()) {
-			resp.setTxnId(txId.get().substring("Ref: ".length()));
-		}
-		for (int i = split.length - 1; i >= 0; i--) {
-			String str = split[i].trim();
-			if (str.startsWith("For") && str.contains(":")) {
-				resp.setMerchant(str.substring(3, str.indexOf(':') - 1).trim());
-				break;
+		if (invoiceInfoRes.getAmmount() == null) {
+			Optional<String> total = Arrays.stream(split).filter(str -> str.startsWith("Total Price: ")).findFirst();
+			if (total.isPresent()) {
+				invoiceInfoRes.setAmmount(total.get().substring("Total Price: ".length()));
 			}
 		}
-		return resp;
+
+		if (invoiceInfoRes.getTxnId() == null) {
+			Optional<String> txId = Arrays.stream(split).filter(str -> str.startsWith("Ref: ")).findFirst();
+			if (txId.isPresent()) {
+				invoiceInfoRes.setTxnId(txId.get().substring("Ref: ".length()));
+			}
+		}
+
+		if (invoiceInfoRes.getMerchant() == null) {
+			for (int i = split.length - 1; i >= 0; i--) {
+				String str = split[i].trim();
+				if (str.startsWith("For") && str.contains(":")) {
+					invoiceInfoRes.setMerchant(str.substring(3, str.indexOf(':') - 1).trim());
+					break;
+				}
+			}
+		}
 	}
 
 }
